f208f7abb4d8bd138c4af2ac9f95874c5d8405b2
Matti Ahinko <matti.ahinko@gofore.com>
2017-08-09 23:44:30 +0300

    Add status icon

--- battery.py
+++ battery.py
@@ -1,8 +1,29 @@
 #!/bin/python3
 import subprocess
 
+def get_status_icon(percent):
+    if percent > 80:
+        return ''
+    if percent > 60:
+        return ''
+    if percent > 30:
+        return ''
+    if percent > 10:
+        return ''
+    return ''
+
 status = subprocess.getoutput('acpi')
-formatted = status.replace('Charging', '').replace('Discharging', '').replace('until charged', '').replace('remaining', '').replace('Battery 0:', '').replace(',', '')
+splitted = status.split(' ')
+percent = int(splitted[3].replace('%,', ''))
+status_icon = get_status_icon(percent)
+
+formatted = status.replace(
+    'Charging', '').replace(
+    'Discharging', status_icon).replace(
+    'until charged', '').replace(
+    'remaining', '').replace(
+    'Battery 0:', '').replace(
+    ',', '')
 print(formatted)
 
 # status = '' if v['POWER_SUPPLY_STATUS'] is 'Charging' else ''
