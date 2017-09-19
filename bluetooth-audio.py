4cd895a2eecc04074e2ef28709080b01ad523028
Matti Ahinko <matti.ahinko@gmail.com>
2017-09-19 21:03:14 +0300

    Add script for handling bluetooth headset

---
+++ bluetooth-audio.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+import subprocess
+import sys
+
+device = '00:16:94:1A:C9:AA'
+
+command = 'bt-device -i {0} | grep -c "Connected: 1"'.format(device)
+connected = int(subprocess.getoutput(command)) > 0
+
+if len(sys.argv) > 1 and sys.argv[1] == '1':
+    command = 'bt-device -{0} {1}'.format('d' if connected else 'c', device)
+    subprocess.getoutput(command)
+
+print('' if connected else '<span color="#4f4f4f"></span>')
